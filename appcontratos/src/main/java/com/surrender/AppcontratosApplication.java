07bfc262990b51ccd9da1f6ae83d160474752c1e
Jose Cabrera <jcabrera9409@gmail.com>
2025-01-11 15:23:01 -0500

    .

--- appcontratos/src/main/java/com/surrender/AppcontratosApplication.java
+++ appcontratos/src/main/java/com/surrender/AppcontratosApplication.java
@@ -2,12 +2,19 @@ package com.surrender;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AppcontratosApplication {
+public class AppcontratosApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppcontratosApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AppcontratosApplication.class);
 	}
 
 }
